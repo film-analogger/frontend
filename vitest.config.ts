d22fc72c5834f1aa38f8902cbed19bfacb9ffcd3
Théo GUERIN <t.guerin@legal2digital.fr>
2026-02-22 12:53:50 +0100

    add github coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
         coverage: {
             provider: 'v8',
             include: ['src/**/*.{ts,tsx}'],
+            reporter: ['text', 'json', 'json-summary'],
         },
         reporters: ['default', 'junit'],
         outputFile: {
