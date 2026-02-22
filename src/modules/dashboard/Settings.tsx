8b2694333dee86563fe168fcacff5996c8187763
Théo GUERIN <t.guerin@legal2digital.fr>
2026-02-22 17:32:16 +0100

    i18n lint

--- src/modules/dashboard/Settings.tsx
+++ src/modules/dashboard/Settings.tsx
@@ -1,10 +1,12 @@
 import { Box, Typography } from '@mui/material';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 const Settings: React.FunctionComponent = () => {
+    const { t } = useTranslation();
     return (
         <Box>
-            <Typography variant="h1">Settings</Typography>
+            <Typography variant="h1">{t('settings.title')}</Typography>
         </Box>
     );
 };
